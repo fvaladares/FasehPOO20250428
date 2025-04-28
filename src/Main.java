a684e4eb77793827c46fe377ac04798cda6cc6cc
Fabricio Valadares <fabricio.valadares@gmail.com>
2025-04-28 20:47:36 -0300

    Modificações nas classes Veiculo.java e Main.java;
    O veículo agora sobrescreve o método tostring;
    
    Este primeiro exemplo está simples e ainda não segue todos os princípios da orientação a objetos.

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,40 @@
 public class Main {
     // Ponto de parteida para a execução do programa
     public static void main(String[] args) {
+        // Criação de um objeto do tipo Veiculo
+        // Veiculo = fusca; -> declaração de variável
+        // fusca = new Veiculo; -> Cria a instância (objeto)
+        Veiculo fusca = new Veiculo();
+        Veiculo kombi; // Declaração de váriavel do tipo Veiculo (Kombi não existe);
+        double distanciaMaxima; // Informa a distância máxima que pode ser percorrida com o combustível
+
+        kombi = new Veiculo(); // Criação do objeto
+
+        fusca.qtdPassageiros = 10;
+        fusca.consumoQuilometrosPorLitro = 25;
+        fusca.capacidadeCombustivel = 50;
+
+        kombi.qtdPassageiros = 10;
+        kombi.consumoQuilometrosPorLitro = 15;
+        kombi.capacidadeCombustivel = 50;
+
+        System.out.println(fusca);
+        System.out.println("+++++++++++");
+        System.out.println("Fusca: " + fusca.qtdPassageiros +
+                " passageiros");
+        System.out.println("Kombi: " + kombi.consumoQuilometrosPorLitro +
+                " Km/L");
+
+        System.out.println("----------");
+        System.out.println("ping...");
+        System.out.println(kombi.ping());
+
+        distanciaMaxima = fusca.capacidadeCombustivel * fusca.consumoQuilometrosPorLitro;
+
+        System.out.println("O fusca ainda pode percorrer " +
+                distanciaMaxima + "KM.");
+
+
 
     }
 }
--- src/Veiculo.java
+++ src/Veiculo.java
@@ -1,2 +1,19 @@
 public class Veiculo {
+    // Definição de atributos da classe
+    int qtdPassageiros;
+    int capacidadeCombustivel;
+    double consumoQuilometrosPorLitro;
+
+    public String ping() {
+        return "pong";
+    }
+
+    @Override
+    public String toString() {
+        return "Veiculo{" +
+                "qtdPassageiros=" + qtdPassageiros +
+                ", capacidadeCombustivel=" + capacidadeCombustivel +
+                ", consumoQuilometrosPorLitro=" + consumoQuilometrosPorLitro +
+                '}';
+    }
 }
