96a92942f07277bd3f0413dcfd5d7ecaa6a070b0
Fabricio Valadares <fabricio.valadares@gmail.com>
2025-04-28 21:15:29 -0300

    Exemplo de atribuição de tipo primitivo e tipo abstrato.
    Tipo primitivo há cópia de valores;
    Tipo abstrato há cópia do endereço de memória, ou seja, as duas variáveis apontam para o mesmo local!

--- src/Main.java
+++ src/Main.java
@@ -3,40 +3,50 @@
 public class Main {
     // Ponto de parteida para a execução do programa
     public static void main(String[] args) {
-        // Criação de um objeto do tipo Veiculo
-        // Veiculo = fusca; -> declaração de variável
-        // fusca = new Veiculo; -> Cria a instância (objeto)
-        Veiculo fusca = new Veiculo();
-        Veiculo kombi; // Declaração de váriavel do tipo Veiculo (Kombi não existe);
-        double distanciaMaxima; // Informa a distância máxima que pode ser percorrida com o combustível
+        int variavelQualquer = 10;
+        int variavelQualquerDois;
+        Veiculo carroUm = new Veiculo();
+        Veiculo carroDois = new Veiculo();
 
-        kombi = new Veiculo(); // Criação do objeto
+        variavelQualquerDois = 0; // inicialização da variável.
 
-        fusca.qtdPassageiros = 10;
-        fusca.consumoQuilometrosPorLitro = 25;
-        fusca.capacidadeCombustivel = 50;
+        System.out.println("Valor da variavelQualquer = " + variavelQualquer);
+        System.out.println("Valor da variavelQualquerDois = " + variavelQualquerDois);
 
-        kombi.qtdPassageiros = 10;
-        kombi.consumoQuilometrosPorLitro = 15;
-        kombi.capacidadeCombustivel = 50;
+        System.out.println();
 
-        System.out.println(fusca);
-        System.out.println("+++++++++++");
-        System.out.println("Fusca: " + fusca.qtdPassageiros +
-                " passageiros");
-        System.out.println("Kombi: " + kombi.consumoQuilometrosPorLitro +
-                " Km/L");
+        carroUm.capacidadeCombustivel = 45;
+        carroUm.qtdPassageiros = 5;
+        carroUm.consumoQuilometrosPorLitro = 13;
 
-        System.out.println("----------");
-        System.out.println("ping...");
-        System.out.println(kombi.ping());
+        System.out.println("Valor do carroUm = " + carroUm);
+        System.out.println("Valor do carroDois = " + carroDois);
 
-        distanciaMaxima = fusca.capacidadeCombustivel * fusca.consumoQuilometrosPorLitro;
+        System.out.println();
 
-        System.out.println("O fusca ainda pode percorrer " +
-                distanciaMaxima + "KM.");
+        variavelQualquerDois = variavelQualquer;
+        variavelQualquer = 20;
 
+        carroDois = carroUm;
 
+        System.out.println("Valor da variavelQualquer = " + variavelQualquer);
+        System.out.println("Valor da variavelQualquerDois = " + variavelQualquerDois);
+
+        System.out.println();
+
+        System.out.println("Valor do carroUm = " + carroUm);
+        System.out.println("Valor do carroDois = " + carroDois);
+
+        System.out.println();
+
+        carroDois.consumoQuilometrosPorLitro = 17;
+
+        System.out.println();
+
+        System.out.println("Valor do carroUm = " + carroUm);
+        System.out.println("Valor do carroDois = " + carroDois);
+
+        System.out.println();
 
     }
 }
